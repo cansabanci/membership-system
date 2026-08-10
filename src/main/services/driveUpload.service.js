7a7a47a3ef23e1cb7912a1c18fb52652cfd3f951
Can Sabanci <mcan.sabanci9@gmail.com>
2026-08-10 15:40:01 +0300

    Google Drive yedekleme: klasör paylaşımıyla uyumlu olması için 'drive.file' yerine 'drive' kapsamı kullan, yeni paylaşılan klasöre geçiş yap

--- src/main/services/driveUpload.service.js
+++ src/main/services/driveUpload.service.js
@@ -2,12 +2,14 @@ const fs = require('fs');
 const { google } = require('googleapis');
 const { credentialsPath } = require('../paths');
 
-const FOLDER_ID = '15cQAlJ_iJobSO_II52Kd9alXBZJElfV-';
+const FOLDER_ID = '1fFjRiP9QXLU4UJCzWD6RqdMg9o4jNX4j';
 
 async function uploadToDrive(filePath, fileName) {
   const auth = new google.auth.GoogleAuth({
     keyFile: credentialsPath,
-    scopes: ['https://www.googleapis.com/auth/drive.file'],
+    // "drive.file" kapsamı yalnızca uygulamanın kendi oluşturduğu dosyaları görebiliyor;
+    // normal Paylaş menüsüyle paylaşılan bir klasöre erişmek için geniş "drive" kapsamı gerekiyor.
+    scopes: ['https://www.googleapis.com/auth/drive'],
   });
 
   const drive = google.drive({ version: 'v3', auth });
